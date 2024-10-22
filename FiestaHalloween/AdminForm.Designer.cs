19ae342746c55eebb1f49ecce959473e6803e855
karensanchez6 <karen.s17.07.04@gmail.com>
2024-10-22 13:52:44 -0500

    creacion de la interfaz inicial

---
+++ FiestaHalloween/AdminForm.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace FiestaHalloween
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerarCodigo = new System.Windows.Forms.Button();
+            this.txtCantidadCodigos = new System.Windows.Forms.NumericUpDown();
+            this.lbxListaCodigos = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbxInivitadosFiesta = new System.Windows.Forms.ListBox();
+            this.btnRegistrarDisfraz = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cbxTipoDulce = new System.Windows.Forms.ComboBox();
+            this.txtCantidadDulce = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAgregarDulces = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAgregarDulce = new System.Windows.Forms.TextBox();
+            this.btnAgregarDulce = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCantidadCodigos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCantidadDulce)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese cantidad de códigos a generar:";
+            // 
+            // btnGenerarCodigo
+            // 
+            this.btnGenerarCodigo.Location = new System.Drawing.Point(52, 107);
+            this.btnGenerarCodigo.Name = "btnGenerarCodigo";
+            this.btnGenerarCodigo.Size = new System.Drawing.Size(131, 43);
+            this.btnGenerarCodigo.TabIndex = 1;
+            this.btnGenerarCodigo.Text = "Generar código de invitación";
+            this.btnGenerarCodigo.UseVisualStyleBackColor = true;
+            // 
+            // txtCantidadCodigos
+            // 
+            this.txtCantidadCodigos.Location = new System.Drawing.Point(52, 79);
+            this.txtCantidadCodigos.Name = "txtCantidadCodigos";
+            this.txtCantidadCodigos.Size = new System.Drawing.Size(131, 22);
+            this.txtCantidadCodigos.TabIndex = 2;
+            // 
+            // lbxListaCodigos
+            // 
+            this.lbxListaCodigos.FormattingEnabled = true;
+            this.lbxListaCodigos.ItemHeight = 16;
+            this.lbxListaCodigos.Location = new System.Drawing.Point(52, 172);
+            this.lbxListaCodigos.Name = "lbxListaCodigos";
+            this.lbxListaCodigos.Size = new System.Drawing.Size(131, 132);
+            this.lbxListaCodigos.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(290, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Registro al concurso de disfraces";
+            // 
+            // lbxInivitadosFiesta
+            // 
+            this.lbxInivitadosFiesta.FormattingEnabled = true;
+            this.lbxInivitadosFiesta.ItemHeight = 16;
+            this.lbxInivitadosFiesta.Location = new System.Drawing.Point(327, 79);
+            this.lbxInivitadosFiesta.Name = "lbxInivitadosFiesta";
+            this.lbxInivitadosFiesta.Size = new System.Drawing.Size(139, 196);
+            this.lbxInivitadosFiesta.TabIndex = 5;
+            // 
+            // btnRegistrarDisfraz
+            // 
+            this.btnRegistrarDisfraz.Location = new System.Drawing.Point(352, 281);
+            this.btnRegistrarDisfraz.Name = "btnRegistrarDisfraz";
+            this.btnRegistrarDisfraz.Size = new System.Drawing.Size(81, 26);
+            this.btnRegistrarDisfraz.TabIndex = 6;
+            this.btnRegistrarDisfraz.Text = "Registrar";
+            this.btnRegistrarDisfraz.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(525, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Creación-Administración de paradas";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(584, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 44);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Crear nueva parada";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(578, 143);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(126, 116);
+            this.listBox1.TabIndex = 9;
+            // 
+            // cbxTipoDulce
+            // 
+            this.cbxTipoDulce.FormattingEnabled = true;
+            this.cbxTipoDulce.Location = new System.Drawing.Point(830, 142);
+            this.cbxTipoDulce.Name = "cbxTipoDulce";
+            this.cbxTipoDulce.Size = new System.Drawing.Size(121, 24);
+            this.cbxTipoDulce.TabIndex = 10;
+            // 
+            // txtCantidadDulce
+            // 
+            this.txtCantidadDulce.Location = new System.Drawing.Point(824, 79);
+            this.txtCantidadDulce.Name = "txtCantidadDulce";
+            this.txtCantidadDulce.Size = new System.Drawing.Size(121, 22);
+            this.txtCantidadDulce.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(833, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Tipo de dulce:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(783, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Cantidad de dulces a agregar";
+            // 
+            // btnAgregarDulces
+            // 
+            this.btnAgregarDulces.Location = new System.Drawing.Point(838, 177);
+            this.btnAgregarDulces.Name = "btnAgregarDulces";
+            this.btnAgregarDulces.Size = new System.Drawing.Size(102, 52);
+            this.btnAgregarDulces.TabIndex = 14;
+            this.btnAgregarDulces.Text = "Agregar dulces";
+            this.btnAgregarDulces.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(569, 291);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Agregar nuevo dulce:";
+            // 
+            // txtAgregarDulce
+            // 
+            this.txtAgregarDulce.Location = new System.Drawing.Point(572, 320);
+            this.txtAgregarDulce.Name = "txtAgregarDulce";
+            this.txtAgregarDulce.Size = new System.Drawing.Size(143, 22);
+            this.txtAgregarDulce.TabIndex = 16;
+            // 
+            // btnAgregarDulce
+            // 
+            this.btnAgregarDulce.Location = new System.Drawing.Point(584, 357);
+            this.btnAgregarDulce.Name = "btnAgregarDulce";
+            this.btnAgregarDulce.Size = new System.Drawing.Size(110, 45);
+            this.btnAgregarDulce.TabIndex = 17;
+            this.btnAgregarDulce.Text = "Agregar Dulce";
+            this.btnAgregarDulce.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 471);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 52);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Cerrar sesión";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 559);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAgregarDulce);
+            this.Controls.Add(this.txtAgregarDulce);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnAgregarDulces);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCantidadDulce);
+            this.Controls.Add(this.cbxTipoDulce);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRegistrarDisfraz);
+            this.Controls.Add(this.lbxInivitadosFiesta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbxListaCodigos);
+            this.Controls.Add(this.txtCantidadCodigos);
+            this.Controls.Add(this.btnGenerarCodigo);
+            this.Controls.Add(this.label1);
+            this.Name = "AdminForm";
+            this.Text = "AdminForm";
+            ((System.ComponentModel.ISupportInitialize)(this.txtCantidadCodigos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCantidadDulce)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerarCodigo;
+        private System.Windows.Forms.NumericUpDown txtCantidadCodigos;
+        private System.Windows.Forms.ListBox lbxListaCodigos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbxInivitadosFiesta;
+        private System.Windows.Forms.Button btnRegistrarDisfraz;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox cbxTipoDulce;
+        private System.Windows.Forms.NumericUpDown txtCantidadDulce;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAgregarDulces;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAgregarDulce;
+        private System.Windows.Forms.Button btnAgregarDulce;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ FiestaHalloween/Disfraces.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace FiestaHalloween
+{
+    partial class Disfraces
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxParticipantes = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pcbDisfraz = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPuntajeDisfraz = new System.Windows.Forms.NumericUpDown();
+            this.btnCalificar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPuntuaciones = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbDisfraz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPuntajeDisfraz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbxParticipantes
+            // 
+            this.lbxParticipantes.FormattingEnabled = true;
+            this.lbxParticipantes.ItemHeight = 16;
+            this.lbxParticipantes.Location = new System.Drawing.Point(29, 61);
+            this.lbxParticipantes.Name = "lbxParticipantes";
+            this.lbxParticipantes.Size = new System.Drawing.Size(140, 212);
+            this.lbxParticipantes.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Participantes:";
+            // 
+            // pcbDisfraz
+            // 
+            this.pcbDisfraz.Location = new System.Drawing.Point(253, 61);
+            this.pcbDisfraz.Name = "pcbDisfraz";
+            this.pcbDisfraz.Size = new System.Drawing.Size(196, 214);
+            this.pcbDisfraz.TabIndex = 2;
+            this.pcbDisfraz.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ver disfraz";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtPuntajeDisfraz
+            // 
+            this.txtPuntajeDisfraz.Location = new System.Drawing.Point(280, 313);
+            this.txtPuntajeDisfraz.Name = "txtPuntajeDisfraz";
+            this.txtPuntajeDisfraz.Size = new System.Drawing.Size(145, 22);
+            this.txtPuntajeDisfraz.TabIndex = 4;
+            this.txtPuntajeDisfraz.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // btnCalificar
+            // 
+            this.btnCalificar.Location = new System.Drawing.Point(298, 341);
+            this.btnCalificar.Name = "btnCalificar";
+            this.btnCalificar.Size = new System.Drawing.Size(112, 27);
+            this.btnCalificar.TabIndex = 5;
+            this.btnCalificar.Text = "calificar";
+            this.btnCalificar.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(304, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Puntaje disfraz:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tabla de puntaciones";
+            // 
+            // lblPuntuaciones
+            // 
+            this.lblPuntuaciones.AutoSize = true;
+            this.lblPuntuaciones.Location = new System.Drawing.Point(552, 91);
+            this.lblPuntuaciones.Name = "lblPuntuaciones";
+            this.lblPuntuaciones.Size = new System.Drawing.Size(44, 16);
+            this.lblPuntuaciones.TabIndex = 8;
+            this.lblPuntuaciones.Text = "label4";
+            // 
+            // Disfraces
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 450);
+            this.Controls.Add(this.lblPuntuaciones);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalificar);
+            this.Controls.Add(this.txtPuntajeDisfraz);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pcbDisfraz);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxParticipantes);
+            this.Name = "Disfraces";
+            this.Text = "Disfraces";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbDisfraz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPuntajeDisfraz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxParticipantes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pcbDisfraz;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown txtPuntajeDisfraz;
+        private System.Windows.Forms.Button btnCalificar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPuntuaciones;
+    }
+}
