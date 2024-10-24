6b90e7d445cde42e6ec00390862ef9b6d97e012b
karensanchez6 <karen.s17.07.04@gmail.com>
2024-10-23 23:07:30 -0500

    plp form actividades

--- FiestaHalloween/FormActividad.Designer.cs
+++ FiestaHalloween/FormActividad.Designer.cs
@@ -32,57 +32,102 @@
             this.btnDulceTruco = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.btnSalir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // btnConcursoDisfraces
             // 
+            this.btnConcursoDisfraces.BackColor = System.Drawing.Color.Yellow;
+            this.btnConcursoDisfraces.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnConcursoDisfraces.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConcursoDisfraces.Location = new System.Drawing.Point(109, 143);
             this.btnConcursoDisfraces.Name = "btnConcursoDisfraces";
             this.btnConcursoDisfraces.Size = new System.Drawing.Size(125, 86);
             this.btnConcursoDisfraces.TabIndex = 0;
             this.btnConcursoDisfraces.Text = "Concurso de disfraces";
-            this.btnConcursoDisfraces.UseVisualStyleBackColor = true;
+            this.btnConcursoDisfraces.UseVisualStyleBackColor = false;
             // 
             // btnDulceTruco
             // 
+            this.btnDulceTruco.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnDulceTruco.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnDulceTruco.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDulceTruco.Location = new System.Drawing.Point(338, 143);
             this.btnDulceTruco.Name = "btnDulceTruco";
             this.btnDulceTruco.Size = new System.Drawing.Size(125, 86);
             this.btnDulceTruco.TabIndex = 1;
             this.btnDulceTruco.Text = "Dulce o Truco";
-            this.btnDulceTruco.UseVisualStyleBackColor = true;
+            this.btnDulceTruco.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(127, 74);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Old English Text MT", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(311, 20);
+            this.label1.Size = new System.Drawing.Size(435, 33);
             this.label1.TabIndex = 2;
             this.label1.Text = "Seleccione en que desea participar:";
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(249, 274);
+            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(94)))), ((int)(((byte)(20)))));
+            this.btnSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Old English Text MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(244, 274);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.Size = new System.Drawing.Size(81, 34);
             this.btnSalir.TabIndex = 3;
             this.btnSalir.Text = "salir";
-            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::FiestaHalloween.Properties.Resources.toallin_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(-22, 125);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 236);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::FiestaHalloween.Properties.Resources.candybag;
+            this.pictureBox2.Location = new System.Drawing.Point(450, 174);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 134);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
             // 
             // FormActividad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FiestaHalloween.Properties.Resources.actividad;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(604, 356);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnDulceTruco);
             this.Controls.Add(this.btnConcursoDisfraces);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormActividad";
             this.Text = "FormActividad";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,5 +139,7 @@
         private System.Windows.Forms.Button btnDulceTruco;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
